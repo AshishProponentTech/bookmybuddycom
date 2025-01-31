9dc2e73f077d3ea40eef12e986928daae2dea783
ashish@proponenttech.com <Ashishtechdeveloper@gmail.com>
2025-01-31 07:46:34 +0530

    flowbite installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
 import Signin from './Pages/Signin'
+import Header from './Components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
